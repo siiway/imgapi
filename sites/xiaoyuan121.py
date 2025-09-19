bf1b200714db6aaf1fdc1c1f5444fd2a2c20e7b2
白白的群子 <bbdqz_114@163.com>
2025-09-19 19:12:30 +0800

    Fix in xiaoyuan121.py

--- sites/xiaoyuan121.py
+++ sites/xiaoyuan121.py
@@ -7,7 +7,7 @@ powered by xiaoyuan
 
 from flask import Blueprint, redirect
 
-allow_s = False
+allow_s = True
 allow_h = True
 allow_v = False
 
@@ -18,4 +18,5 @@ site = Blueprint('xiaoyuan', __name__)
 @site.route('/image')
 @site.route('/image/h')
 def image_h():
+
     return redirect('https://img.xiaoyuan151.com/neuro')
